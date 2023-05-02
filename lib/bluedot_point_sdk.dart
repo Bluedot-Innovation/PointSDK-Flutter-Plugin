822e53b9b13c4a057970eb784f92ccb8f5003aa9
Danny Nguyen <danny@bluedot.io>
2023-05-02 10:36:56 +0930

    update function docs

--- lib/bluedot_point_sdk.dart
+++ lib/bluedot_point_sdk.dart
@@ -11,9 +11,11 @@ class BluedotPointSdk {
   /// The singleton instance of Bluedot Point SDK
   static final instance = BluedotPointSdk();
 
-  /// Initialize SDK with [projectId]. You can find your [projectId] on Bluedot Canvas.
+  /// Initialize SDK with [projectId]. You can find your [projectId] on Bluedot
+  /// Canvas.
   ///
-  /// If the initialization is successful, error will be returned as nil. However, if the initialization fails, an error will be provided.
+  /// If the initialization is successful, error will be returned as nil.
+  /// However, if the initialization fails, an error will be provided.
   Future<void> initialize(String projectId) {
     return BluedotPointSdkPlatform.instance.initialize(projectId);
   }
@@ -41,31 +43,48 @@ class BluedotPointSdk {
 
   /// Stop GeoTriggering features of the Bluedot Point SDK
   ///
-  /// Stopping Geo-triggering feature has the intended effect of stopping location services on the device, thereby conserving battery on your user’s device unless another feature such as Tempo, is active.
-  /// If the Geo-Triggering feature is stopped successful, error will be returned as nil. However, if the Stop Geo-Triggering fails, an error will be provided.
+  /// Stopping Geo-triggering feature has the intended effect of stopping
+  /// location services on the device, thereby conserving battery on your
+  /// user’s device unless another feature such as Tempo, is active.
+  /// If the Geo-Triggering feature is stopped successful, error will be
+  /// returned as nil. However, if the Stop Geo-Triggering fails, an error
+  /// will be provided.
   Future<void> stopGeoTriggering() {
     return BluedotPointSdkPlatform.instance.stopGeoTriggering();
   }
 
   /// Stop Tempo Tracking
   ///
-  /// If the Tempo is stopped successful, error will be returned as nil. However, if the Start Tempo fails, an error will be provided.
+  /// If the Tempo is stopped successful, error will be returned as nil.
+  /// However, if the Start Tempo fails, an error will be provided.
   Future<void> stopTempoTracking() {
     return BluedotPointSdkPlatform.instance.stopTempoTracking();
   }
 
   /// Sets custom metadata for Notification events.
   ///
-  /// Only up to 20 custom meta data fields are allowed. Will throw an exception if the number of custom fields exceeded.
-  /// The custom metadata set through this API will be available on the backend in check-in activity log and via webhooks.
+  /// Only up to 20 custom meta data fields are allowed.
+  /// Will throw an exception if the number of custom fields exceeded.
+  /// The custom metadata set through this API will be available on the backend
+  /// in check-in activity log and via webhooks.
   Future<void> setCustomEventMetaData(Map<String, String> metadata) {
     return BluedotPointSdkPlatform.instance.setCustomEventMetaData(metadata);
   }
 
-  /// By default, PointSDK uses `ic_stat_name` icon for GeoTriggering and Tempo services foreground notification icons.
-  /// Use this method to assign a custom notification icon for Android foreground notification.
-  /// If [icon] is null or doesn't exist in android's res/drawable or res/mipmap,
-  /// the default one `ic_stat_name` will be used. If also not available, then app launcher icon is used.
+  /// By default, PointSDK uses `ic_stat_name` icon in res/drawable resources
+  /// for GeoTriggering and Tempo services foreground notification icons.
+  ///
+  /// Use this method to assign a custom notification icon for Android
+  /// foreground notification. If [icon] is null or doesn't exist in android's
+  /// res/drawable or res/mipmap, the default one `ic_stat_name` will be used.
+  /// If also not available, then app launcher icon is used.
+  ///
+  /// ### IMPORTANT:
+  /// Flutter defaults `shrinkResources` to true for release builds, and the
+  /// [icon] may be deleted from release builds if it's not referenced directly
+  /// from code at build time. Follow Android's documentation
+  /// [keep-resources](https://developer.android.com/build/shrink-code#keep-resources)
+  /// to specify which resources to keep.
   Future<void> setNotificationIcon(String icon) {
     return BluedotPointSdkPlatform.instance.setNotificationIcon(icon);
   }
@@ -77,8 +96,10 @@ class BluedotPointSdk {
 
   /// Returns the installation reference of this Point SDK enabled App.
   ///
-  ///  This is the same as the Install Ref that appears in a Zone's Activity Log in Canvas, or queried via Open API.
-  ///  This reference is randomly generated at the first run-time of the App and remains fixed for the duration of the App installation.
+  /// This is the same as the Install Ref that appears in a Zone's Activity Log
+  /// in Canvas, or queried via Open API.
+  /// This reference is randomly generated at the first run-time of the App and
+  /// remains fixed for the duration of the App installation.
   Future<String> getInstallRef() {
     return BluedotPointSdkPlatform.instance.getInstallRef();
   }
@@ -88,15 +109,18 @@ class BluedotPointSdk {
     return BluedotPointSdkPlatform.instance.getSDKVersion();
   }
 
-  /// Method to get a collection of `BDZoneInfo` objects, corresponding to the Zones you created for this project, in the Canvas interface.
+  /// Method to get a collection of `BDZoneInfo` objects, corresponding to the
+  /// Zones you created for this project, in the Canvas interface.
   Future<dynamic> getZonesAndFences() {
     return BluedotPointSdkPlatform.instance.getZonesAndFences();
   }
 
-  /// Reset Bluedot Point SDK (only if you wish to switch to a different projectId)
+  /// Reset Bluedot PointSDK (only if you wish to switch to a different projectId)
   ///
-  /// Call this method only if you need to switch to a different projectId, this will reset the Bluedot Point SDK to its original state.
-  /// Upon completion callback, you can make the call [initialize(String projectId)]  to initialize with a different projectId.
+  /// Call this method only if you need to switch to a different projectId,
+  /// this will reset the Bluedot Point SDK to its original state.
+  /// Upon completion callback, you can call [initialize(String projectId)]
+  /// to initialize with a different projectId.
   Future<void> reset() {
     return BluedotPointSdkPlatform.instance.reset();
   }
@@ -113,10 +137,20 @@ class BluedotPointSdk {
 
   /// Enable or disable background location updates (iOS only)
   ///
-  /// For the background location usage indicator to work, allowsBackgroundLocationUpdates must be set to true while the app is in the foreground, and the app has While using the app location authorization.
-  /// If allowsBackgroundLocationUpdates is set to true while the app is in the background, or the user changes the location permission to While using the app while the app is in the background, the background location usage indicator will not be enabled.
-  /// The default value of allowsBackgroundLocationUpdates is false, and it can be disabled while the app is either in the foreground or the background.
-  /// If the application requests Always location authorization, be sure to check that Always location authorization has not been granted before setting allowsBackgroundLocationUpdates to false, as setting the value to false will prevent the app from accessing location from the background.
+  /// For the background location usage indicator to work,
+  /// `allowsBackgroundLocationUpdates` must be set to `true` while the app is in
+  /// the foreground, and the app has `While using` location authorization.
+  /// If `allowsBackgroundLocationUpdates` is set to `true` while the app is in
+  /// the background, or the user changes the location permission to `While using`
+  /// while the app is in the background, the background location usage
+  /// indicator will not be enabled.
+  ///
+  /// The default value of `allowsBackgroundLocationUpdates` is `false`, and it
+  /// can be disabled while the app is either in the foreground or background.
+  /// If the application requests `Always` location authorization, be sure to
+  /// check that `Always` location authorization has not been granted before
+  /// setting `allowsBackgroundLocationUpdates` to `false`, as setting the value
+  /// to `false` will prevent the app from accessing location from the background.
   void allowsBackgroundLocationUpdates(bool value) {
     return BluedotPointSdkPlatform.instance.allowsBackgroundLocationUpdates(value);
   }
