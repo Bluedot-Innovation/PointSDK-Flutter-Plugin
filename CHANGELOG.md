17ba953288ddcd64f61608ce772e22474bd23c20
Natalia Klymenko <natalia@bluedot.io>
2026-03-05 16:25:25 +1100

    BD-7333 Update iOS SDK with v 17.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Flutter Bluedot Point SDK Plugin release notes
 
 ## 2.1.1
-- Updated to Android Point SDK v17.4.1 and iOS Point SDK v17.1.0
+- Updated to Android Point SDK v17.4.1 and iOS Point SDK v17.2.0
 
 ## 2.1.0
 - Updated to Android Point SDK v17.3.0 and iOS Point SDK v17.1.0
--- ios/Classes/SwiftBluedotPointSdkPlugin.swift
+++ ios/Classes/SwiftBluedotPointSdkPlugin.swift
@@ -246,7 +246,12 @@ extension SwiftBluedotPointSdkPlugin: BDPGeoTriggeringEventDelegate {
         sendEvent(eventName: "didExitZone", modelName: "GeoTriggerEvent", jsonStr: json)
     }
 
-    // Use Dart to parse the json string and pass the resulting object to the
+    public func didDwellInZone(_ dwellEvent: GeoTriggerEvent) {
+        let json = (try? dwellEvent.toJson() as String?) ?? ""
+        sendEvent(eventName: "didDwellInZone", modelName: "GeoTriggerEvent", jsonStr: json)
+    }
+
+   // Use Dart to parse the json string and pass the resulting object to the
     // client callback.
     private func sendEvent(eventName: String, modelName: String, jsonStr: String) -> Any {
         self.geoTriggeringUtilsChannel?.invokeMethod(
